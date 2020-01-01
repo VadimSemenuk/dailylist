bc4d0d2234cedb8e4080ffe387384c4be1e23556
Vadim Semenyuk <vadim54787@gmail.com>
2020-01-01 23:19:54 +0300

    fixes

--- app/src/main/java/com/mamindeveloper/dailylist/AboutActivity.java
+++ app/src/main/java/com/mamindeveloper/dailylist/AboutActivity.java
@@ -3,6 +3,9 @@ package com.mamindeveloper.dailylist;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.text.Html;
 import android.text.SpannableString;
@@ -10,6 +13,7 @@ import android.text.Spanned;
 import android.text.method.LinkMovementMethod;
 import android.text.style.UnderlineSpan;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
 public class AboutActivity extends AppCompatActivity {
@@ -33,6 +37,13 @@ public class AboutActivity extends AppCompatActivity {
         content.setSpan(new UnderlineSpan(), 0, content.length(), 0);
         feedback.setText(content);
         feedback.setMovementMethod(LinkMovementMethod.getInstance());
+
+        findViewById(R.id.rate_app).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openAppInGooglePlay();
+            }
+        });
     }
 
     @Override
@@ -43,6 +54,20 @@ public class AboutActivity extends AppCompatActivity {
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void openAppInGooglePlay() {
+        Uri uri = Uri.parse("market://details?id=" + getPackageName());
+        Intent goToMarket = new Intent(Intent.ACTION_VIEW, uri);
+        goToMarket.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY |
+                Intent.FLAG_ACTIVITY_NEW_DOCUMENT |
+                Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
+        try {
+            startActivity(goToMarket);
+        } catch (ActivityNotFoundException e) {
+            startActivity(new Intent(Intent.ACTION_VIEW,
+                    Uri.parse("http://play.google.com/store/apps/details?id=" + getPackageName())));
         }
     }
 }
